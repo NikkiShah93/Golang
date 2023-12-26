b60f9bceb744dbad0f5ff6346d8bc0c61c716634
Negar Shahrokh <neg.shahrokh@gmail.com>
2023-12-25 21:40:43 -0500

    using formatted string in go

--- src/github.com/NikkiShah93/firstapp/Main.go
+++ src/github.com/NikkiShah93/firstapp/Main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// if you're defining a var outside main
+// you have to declare the type
+var t int = 8
+
 func main() {
 	// use this when you don't want
 	// want to initilaze the var when defining it
@@ -14,7 +18,13 @@ func main() {
 	// use this when go has all the info
 	// required to identify the type of data
 	k := 42
+	// you can't initilaze float32 using ^
+	// because go will infer the numbers
+	// to be int
+	// or float64 if there's a decimal place
 	fmt.Println(i)
 	fmt.Println(j)
 	fmt.Println(k)
+	// use printf for formatted strings
+	fmt.Printf("value is %v and type is %T", j, j)
 }
