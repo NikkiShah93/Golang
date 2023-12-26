b08a8ce35149c8c967c0c51b15c56666f7dbc5a1
Negar Shahrokh <neg.shahrokh@gmail.com>
2023-12-25 21:32:59 -0500

    testing with different types of var assignment

--- src/github.com/NikkiShah93/firstapp/Main.go
+++ src/github.com/NikkiShah93/firstapp/Main.go
@@ -3,8 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var i int = 42
+	// use this when you don't want
+	// want to initilaze the var when defining it
+	// and assign values later, maybe in a loop
+	var i int
+	i = 7
+	// use this when go doesn't have enough info
+	// to identify the type of data
+	var j int = 99
+	// use this when go has all the info
+	// required to identify the type of data
+	k := 42
 	fmt.Println(i)
-	b := 6
-	fmt.Println(b)
+	fmt.Println(j)
+	fmt.Println(k)
 }
