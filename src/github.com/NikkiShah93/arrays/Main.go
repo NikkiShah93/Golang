541ab0f1bbcb6a15220820c94b9deaa220c6cd3d
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-01-04 20:58:50 -0500

    initial commit

---
+++ src/github.com/NikkiShah93/arrays/Main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// arrays and their use cases
+	// why do we need arrays?
+	// when we need to store multiple related values
+	// it can be cumbersome
+	// so we use arrays instead
+	// and this is how we declare them
+	// we have to start with the size of the array
+	// which is the number of elememts it's gonna hold
+	// and the type of data it's gonna store
+	// so it can only store one type of data
+	grades := [3]int
+
+}
