30a223657cb3c7c7e18a00d44b7e849c355f95d8
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-02-06 16:53:31 -0500

    initial commit

---
+++ src/github.com/NikkiShah93/controlFlow/Main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// this module is on different ways
+// to control the flow of the app
+// and the main topics are
+// if , if-else, if-else-if
+// and the switch statements
+func main() {
+	// lets start with if statements
+	// we start with if keyword
+	// then have a condition
+	// that generates a boolean result
+	// and inside the {} is the code
+	// that'll be executed when
+	// the condition is met
+	if true {
+		fmt.Println("The test is true")
+	}
+}
